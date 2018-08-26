541fc111c1ff2986b79d75508d40a2fa32cc0032
kiborgok <borgokalek14@gmail.com>
2018-08-26 13:03:01 +0300

    new staff

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var fortune = require('./lib/fortune.js');
 var app = express();
 
 // set up handlebars view engine
@@ -9,35 +9,40 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+    });
 
     app.get('/', function(req, res){
-    res.render('home');
-    });
+        res.render('home');
+        });
 
-    app.get('/about', function(req, res){
-        var fortunes = [
-            "Conquer your fears or they will conquer you.",
-            "Rivers need springs.",
-            "Do not fear what you don't know.",
-            "You will have a pleasant surprise.",
-            "Whenever possible, keep it simple.",
-        ];
-        var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-        res.render('about', { fortune: randomFortune });
-    });
+    app.get('/about', function(req, res) {
+        res.render('about', {
+            fortune: fortune.getFortune(), pageTestScript: '/qa/tests-about.js'
+        });
+        });
+
+    app.get('/tours/hood-river', function(req, res){
+        res.render('tours/hood-river');
+        });
+    app.get('/tours/request-group-rate', function(req, res){
+        res.render('tours/request-group-rate');
+        });
 
     // 404 catch-all handler (middleware)
     app.use(function(req, res, next){
-    res.status(404);
-    res.render('404');
-    });
+        res.status(404);
+        res.render('404');
+        });
     // 500 error handler (middleware)
     app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-    });
+        console.error(err.stack);
+        res.status(500);
+        res.render('500');
+        });
 
     app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
-    });
+        console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+        });
